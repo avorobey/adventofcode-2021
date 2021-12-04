3da24481673bb3b60b44664914486697b45f4c84
Anatoly Vorobey <avorobey@gmail.com>
2021-12-05 01:55:35 +0200

    part 2

--- aoc02.zig
+++ aoc02.zig
@@ -21,7 +21,26 @@ pub const Data = struct {
   }
 };
 
-test "test" {
+pub const DataPart2 = struct {
+  depth: u64,
+  horiz: u64,
+  aim: u64,
+  fn processInstruction(self: *DataPart2, ins: []const u8) anyerror!void {
+    if (std.mem.startsWith(u8, ins, forward)) {
+      const x = try std.fmt.parseInt(u64, ins[forward.len..], 10);
+      self.horiz += x;
+      self.depth += x*self.aim;
+    }
+    if (std.mem.startsWith(u8, ins, down)) {
+      self.aim += try std.fmt.parseInt(u64, ins[down.len..], 10);
+    }
+    if (std.mem.startsWith(u8, ins, up)) {
+      self.aim -= try std.fmt.parseInt(u64, ins[up.len..], 10);
+    }
+  }
+};
+
+test "test part1" {
   var d1 = Data{
     .depth = 0,
     .horiz = 0,
@@ -40,6 +59,30 @@ test "test" {
   try expect(d1.depth == 1);
 }
 
+test "test part2" {
+  var d1 = DataPart2{
+    .depth = 0,
+    .horiz = 0,
+    .aim = 0,
+  };
+  try d1.processInstruction("forward 13");
+  try expect(d1.horiz == 13);
+  try expect(d1.depth == 0);
+  try expect(d1.aim == 0);
+  try d1.processInstruction("down 5");
+  try expect(d1.horiz == 13);
+  try expect(d1.depth == 0);
+  try expect(d1.aim == 5);
+  try d1.processInstruction("forward 10");
+  try expect(d1.horiz == 23);
+  try expect(d1.depth == 50);
+  try expect(d1.aim == 5);
+  try d1.processInstruction("up 4");
+  try expect(d1.horiz == 23);
+  try expect(d1.depth == 50);
+  try expect(d1.aim == 1);
+}
+
 pub fn main() anyerror!void {
   var file = try std.fs.cwd().openFile("aoc02.input", .{});
   defer file.close();
@@ -51,8 +94,15 @@ pub fn main() anyerror!void {
     .depth = 0,
     .horiz = 0,
   };
+  var data_part_2 = DataPart2{
+    .depth = 0,
+    .horiz = 0,
+    .aim = 0,
+  };
   while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
     try data.processInstruction(line);
+    try data_part_2.processInstruction(line);
   }
   try std.io.getStdOut().writer().print("part1: {}\n", .{data.depth*data.horiz});
+  try std.io.getStdOut().writer().print("part2: {}\n", .{data_part_2.depth*data_part_2.horiz});
 }
