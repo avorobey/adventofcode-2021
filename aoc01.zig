a2dc41840934efd4b6d8443078c68b1b4fb789f8
Anatoly Vorobey <avorobey@gmail.com>
2021-12-04 18:29:08 +0200

    part 2

--- aoc01.zig
+++ aoc01.zig
@@ -1,14 +1,24 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-fn countIncreases(ar: []i16) u16 {
+fn countIncreases(ar: []i16, window:usize) u16 {
   var prev:i16 = 0;
   var cnt:u16 = 0;
-  for (ar) |depth, index| {
-    if (index != 0 and depth > prev) {
+  for (ar) |_, index| {
+    if (index+window > ar.len) {
+      continue;
+    }
+    // Sum next "window" ar members starting at index
+    var i:usize = 0;
+    var curr:i16 = 0;
+    while (i < window) {
+      curr += ar[index+i];
+      i += 1;
+    }
+    if (index != 0 and curr > prev) {
       cnt += 1;
     }
-    prev = depth;
+    prev = curr;
   }
   return cnt;
 }
@@ -18,10 +28,10 @@ test "test" {
   var a2 = [_]i16{100, 95, 93, 98, 200, 1};
   var a3 = [_]i16{0, 0, 0};
   var a4 = [_]i16{12, 14, 12, 14};
-  try expect(countIncreases(a1[0..])==2);
-  try expect(countIncreases(a2[0..])==2);
-  try expect(countIncreases(a3[0..])==0);
-  try expect(countIncreases(a4[0..])==2);
+  try expect(countIncreases(a1[0..], 1)==2);
+  try expect(countIncreases(a2[0..], 1)==2);
+  try expect(countIncreases(a3[0..], 1)==0);
+  try expect(countIncreases(a4[0..], 1)==2);
 }
 
 pub fn main() anyerror!void {
@@ -37,6 +47,8 @@ pub fn main() anyerror!void {
     depths[cur] = num;
     cur += 1;
   }
-  const res:u16 = countIncreases(depths[0..cur]);
-  try std.io.getStdOut().writer().print("{}\n", .{res});
+  const res1:u16 = countIncreases(depths[0..cur], 1);
+  const res3:u16 = countIncreases(depths[0..cur], 3);
+  try std.io.getStdOut().writer().print("part1: {}\n", .{res1});
+  try std.io.getStdOut().writer().print("part2: {}\n", .{res3});
 }
