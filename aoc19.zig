0563349d14a9bcf528ff78c9a22a189afb0fb3ed
Anatoly Vorobey <avorobey@gmail.com>
2021-12-21 23:28:22 +0200

    part2

--- aoc19.zig
+++ aoc19.zig
@@ -19,6 +19,7 @@ fn compPoint(_: void, p1: Point, p2: Point) bool {
 pub const Scanner = struct {
     buf: [60]Point = undefined,
     len: usize = 0,
+    scanner_pov: Point = undefined,
     fn pr(self: *const Scanner) void {
         std.debug.print("\n", .{});
         for (self.buf[0..self.len]) |p| {
@@ -126,8 +127,8 @@ fn match(first: *Scanner, second: *const Scanner) ?Scanner {
             // val1.pr();
             for (rotated.buf[0..rotated.len]) |val2| {
                 var shifted = rotated.shiftInto(val2, val1);
-                _ = shifted;
                 if (first.countDups(&shifted) >= 12) {
+                    shifted.scanner_pov = Point{ .x = val1.x - val2.x, .y = val1.y - val2.y, .z = val1.z - val2.z };
                     return shifted;
                 }
             }
@@ -171,6 +172,7 @@ pub fn main() anyerror!void {
     var adj_ind: usize = 0;
 
     adjusted[0] = scanners[0];
+    adjusted[0].scanner_pov = Point{ .x = 0, .y = 0, .z = 0 };
     adj_num = 1;
     adj_ind = 0;
     done[0] = true;
@@ -198,4 +200,18 @@ pub fn main() anyerror!void {
     // std.debug.print("adjusted scanners: {d}\n", .{adj_num});
 
     try std.io.getStdOut().writer().print("part1: {}\n", .{countGlobalUniques(adjusted[0..adj_num])});
+
+    var max_dist: i16 = 0;
+    for (adjusted[0..adj_num]) |val1| {
+        for (adjusted[0..adj_num]) |val2| {
+            const x_dist = try std.math.absInt(val1.scanner_pov.x - val2.scanner_pov.x);
+            const y_dist = try std.math.absInt(val1.scanner_pov.y - val2.scanner_pov.y);
+            const z_dist = try std.math.absInt(val1.scanner_pov.z - val2.scanner_pov.z);
+            const dist = x_dist + y_dist + z_dist;
+            if (dist > max_dist) {
+                max_dist = dist;
+            }
+        }
+    }
+    try std.io.getStdOut().writer().print("part2: {}\n", .{max_dist});
 }
